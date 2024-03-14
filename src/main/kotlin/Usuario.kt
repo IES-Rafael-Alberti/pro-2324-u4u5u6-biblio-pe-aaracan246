9676c297646cd2f69e76b10429d0b35b2eb85735
morri <aaracan246@g.educaand.es>
2024-03-14 20:53:33 +0100

    ejercicio-3-clases-contenidov3RegistroPrestamosv3Moviendo de sitio

--- src/main/kotlin/Usuario.kt
+++ src/main/kotlin/Usuario.kt
@@ -22,20 +22,3 @@ class Usuario(id: Int, private val nombre: String): UtilidadesBiblioteca(){
     }
 }
 
-class RegistroPrestamos(): GestorBiblioteca(){
-    private val historialRegistros = mutableMapOf<Libro, Usuario>()
-
-    fun devolverPrestamo(libro: Libro, usuario: Usuario){
-        GestorBiblioteca().catalogoLibros.add(libro)
-        usuario.removeLibro(libro)
-        historialRegistros[libro] = usuario
-    }
-
-    fun prestar(libro: Libro, usuario: Usuario){
-        GestorBiblioteca().catalogoLibros.remove(libro)
-        usuario.addLibro(libro)
-        historialRegistros[libro] = usuario
-    }
-
-
-}
